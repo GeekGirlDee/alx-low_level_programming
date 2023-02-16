615423c35a9ea1f80a8acaa7d2bc1ea2cdbb7f8a
GeekGirlDee <dmabitle839@gmail.com>
2023-02-15 23:40:58 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that will be printing a line with a puts function
+ * Return: 0(Pass)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
