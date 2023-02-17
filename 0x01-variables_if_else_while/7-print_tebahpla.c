07544942ad245427e38e7cd72fedd72f55efd5a4
GeekGirlDee <dmabitle839@gmail.com>
2023-02-17 00:05:05 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints the lowercase alphabet in
+ * reverse, followed by a new line
+ * Return: 0 (pass)
+ */
+int main(void)
+{
+	chat alph = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
