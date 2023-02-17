237d414af7e011400732fb8c05e934d13a90ea20
GeekGirlDee <dmabitle839@gmail.com>
2023-02-17 09:04:22 -0800

    10. Investing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,39 +11,23 @@
  */
 int main(void)
 {
-	int nums;
-	int nums1;
+	int num1;
+	int num2;
 
-	for (nums = '0'; nums < '9'; nums++)
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = numb1 + 1; num2 < 10; num2++)
 		{
-			for (nums1 = '0'; nums1 <= '9'; nums1++)
-				{
-					if (nums == '0' && nums1 < '1')
-						continue;
-					if (nums == '1' && nums1 < '2')
-						continue;
-					if (nums == '2' && nums1 < '3')
-						continue;
-					if (nums == '3' && nums1 < '4')
-						continue;
-					if (nums == '4' && nums1 < '5')
-						continue;
-					if (nums == '5' && nums1 < '6')
-						continue;
-					if (nums == '6' && nums1 < '7')
-						continue;
-					if (nums == '7' && nums1 < '8')
-						continue;
-					if (nums == '8' && nums1 < '9')
-						continue;
-					putchar(nums);
-					putchar(nums1);
-					if (nums == '8' && nums1 == '9')
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
