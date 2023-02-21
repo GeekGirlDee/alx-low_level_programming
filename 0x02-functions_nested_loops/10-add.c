92eb8345172c7c33fd8623b4f54b1c981b7705a1
GeekGirlDee <dmabitle839@gmail.com>
2023-02-21 11:37:35 -0800

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - A program that adds two integers togethr
+ * @a: takes in an integer
+ * @b: takes in an integer
+ * Return: results of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
