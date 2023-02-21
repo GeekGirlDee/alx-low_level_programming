db662ed890e5b6f520f290f349dd2c52b7443f54
GeekGirlDee <dmabitle839@gmail.com>
2023-02-21 04:58:15 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - A program that prints _putchar, followed by a new line
+ * Return: 0 (Pass)
+ */
+int main(void)
+{
+	char alp[] = "_putchar";
+	int num;
+
+	for (num = 0; num < 8; num++)
+	{
+		_putchar(alp[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
