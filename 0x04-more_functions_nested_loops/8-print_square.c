11115765e7864415283419533be2ecbf282a5b2b
GeekGirlDee <dmabitle839@gmail.com>
2023-02-23 14:47:55 -0800

    8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - A program that prints a square, followed by a new line;
+ * if size is 0 or less print only a new line else @size
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
