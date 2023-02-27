051d61fa756885fa3a32b5847630edc6467ead09
GeekGirlDee <dmabitle839@gmail.com>
2023-02-27 15:38:47 +0200

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - prints a string followed by a new line
+ * _puts - Anprogram that prints a string followed by a new line to stdout
  * @str: input
  */
 void _puts(char *str)
