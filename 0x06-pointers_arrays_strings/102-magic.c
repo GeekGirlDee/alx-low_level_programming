2e57905dfc66095997eaf980397a125e4b92ec9e
GeekGirlDee <dmabitle839@gmail.com>
2023-03-01 13:57:12 +0200

    Fixing my code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,17 +5,17 @@
 * you can only write one line
 * You are not allowed to modify the variable p, or use ',',
 * not allowed to code anything expect the line
+* Return: value
 */
 int main(void)
 {
 	int n;
 	int a[5];
-  	int *p;
+	int *p;
 
 	a[2] = 1024;
 	p = &n;
-	/* add the line below
-	*/
+	/* add the line below */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
