aa8ff329589a1670ec66c0f6ec92c735bc47c3eb
GeekGirlDee <dmabitle839@gmail.com>
2023-03-01 13:08:18 +0200

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* Add one line to the code, so that the program prints a[2] = 98,
+* followed by a new line
+* you can only write one line
+* You are not allowed to modify the variable p, or use ',',
+* not allowed to code anything expect the line
+*/
+int main(void)
+{
+	  int n;
+	    int a[5];
+	      int *p;
+
+	        a[2] = 1024;
+		  p = &n;
+		  /* add the line below
+		  */
+		    *(p + 5) = 98;
+		      /* ...so that this prints 98\n */
+		      printf("a[2] = %d\n", a[2]);
+		        return (0);
+}
