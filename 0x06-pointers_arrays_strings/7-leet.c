4fe6007c41221cdc108229e73b78ccaf1998628e
GeekGirlDee <dmabitle839@gmail.com>
2023-03-01 20:14:09 +0200

    Fixing my code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,21 +9,20 @@
  */
 char *leet(char *s)
 {
-	int i, j;
-
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
+	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
+	int i;
+	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; alphaArr[j] != '\0'; j++)
 		{
-			if (s[i] == a[j])
+			if (s[i] == alphaArr[j])
 			{
-				s[i] = b[j];
+				s[i] = alphaArr[j + 1];
+				break;
 			}
 		}
 	}
 	return (s);
 }
-
