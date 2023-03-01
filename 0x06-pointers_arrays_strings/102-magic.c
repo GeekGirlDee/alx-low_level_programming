5073419383f0ca6d41533d274ee03a4b12be2f01
GeekGirlDee <dmabitle839@gmail.com>
2023-03-01 12:29:27 -0800

    Fixing my code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - A function that prints 98\n
- * Return: 0
- */
 
 int main(void)
 {
@@ -13,8 +9,12 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	* Remember: you are not allowed to use a, you are not,
-	* allowed to modify p, only one statement
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
 	*/
 	p[5] = 98;
 	/* ...so that this prints 98\n */
